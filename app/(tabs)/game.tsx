64db9279766aa2c4b837e8daedca320972c9fd63
FOURNIER Quentin <quentin.fournier@etu.univ-amu.fr>
2025-03-27 16:26:50 +0100

    initialisation d'un plateau de jeu

--- app/(tabs)/game.tsx
+++ app/(tabs)/game.tsx
@@ -1,5 +1,7 @@
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, Button, StyleSheet, ScrollView } from 'react-native';
 import { useRouter } from 'expo-router';
+import Card from './../../components/uno/Card';
 
 export default function GameScreen() {
   const router = useRouter();
@@ -8,24 +10,148 @@ export default function GameScreen() {
     router.replace('/');
   };
 
+  // Constante, tableau de toutes les cartes d'un jeu de uno
+  const allCards = [
+    { color: 'red', value: '0', visible: false },
+    { color: 'red', value: '1', visible: false },
+    { color: 'red', value: '2', visible: false },
+    { color: 'red', value: '3', visible: false },
+    { color: 'red', value: '4', visible: false },
+    { color: 'red', value: '5', visible: false },
+    { color: 'red', value: '6', visible: false },
+    { color: 'red', value: '7', visible: false },
+    { color: 'red', value: '8', visible: false },
+    { color: 'red', value: '9', visible: false },
+    { color: 'red', value: 'skip', visible: false },
+    { color: 'red', value: 'reverse', visible: false },
+    { color: 'red', value: 'draw', visible: false },
+    { color: 'green', value: '0', visible: false },
+    { color: 'green', value: '1', visible: false },
+    { color: 'green', value: '2', visible: false },
+    { color: 'green', value: '3', visible: false },
+    { color: 'green', value: '4', visible: false },
+    { color: 'green', value: '5', visible: false },
+    { color: 'green', value: '6', visible: false },
+    { color: 'green', value: '7', visible: false },
+    { color: 'green', value: '8', visible: false },
+    { color: 'green', value: '9', visible: false },
+    { color: 'green', value: 'skip', visible: false },
+    { color: 'green', value: 'reverse', visible: false },
+    { color: 'green', value: 'draw', visible: false },
+    { color: 'blue', value: '0', visible: false },
+    { color: 'blue', value: '1', visible: false },
+    { color: 'blue', value: '2', visible: false },
+    { color: 'blue', value: '3', visible: false },
+    { color: 'blue', value: '4', visible: false },
+    { color: 'blue', value: '5', visible: false },
+    { color: 'blue', value: '6', visible: false },
+    { color: 'blue', value: '7', visible: false },
+    { color: 'blue', value: '8', visible: false },
+    { color: 'blue', value: '9', visible: false },
+    { color: 'blue', value: 'skip', visible: false },
+    { color: 'blue', value: 'reverse', visible: false },
+    { color: 'blue', value: 'draw', visible: false },
+    { color: 'yellow', value: '0', visible: false },
+    { color: 'yellow', value: '1', visible: false },
+    { color: 'yellow', value: '2', visible: false },
+    { color: 'yellow', value: '3', visible: false },
+    { color: 'yellow', value: '4', visible: false },
+    { color: 'yellow', value: '5', visible: false },
+    { color: 'yellow', value: '6', visible: false },
+    { color: 'yellow', value: '7', visible: false },
+    { color: 'yellow', value: '8', visible: false },
+    { color: 'yellow', value: '9', visible: false },
+    { color: 'yellow', value: 'skip', visible: false },
+    { color: 'yellow', value: 'reverse', visible: false },
+    { color: 'yellow', value: 'draw', visible: false },
+    { color: 'wild', value: '', visible: false },
+    { color: 'wild', value: '', visible: false },
+    { color: 'wild', value: '', visible: false },
+    { color: 'wild', value: '', visible: false },
+    { color: 'wild', value: 'draw', visible: false },
+    { color: 'wild', value: 'draw', visible: false },
+    { color: 'wild', value: 'draw', visible: false },
+    { color: 'wild', value: 'draw', visible: false },
+  ];
+
+  // On cree la pioche (mélange les cartes aléatoirement et fais une pile)
+    const deck = allCards.sort(() => Math.random() - 0.5);
+
+    // On distribue les cartes aux joueurs
+    const playerHand = deck.slice(0, 7).map((card) => ({ ...card, visible: true }));
+    const botHand = deck.slice(7, 14).map((card) => ({ ...card, visible: false }));
+    // On enleve les cartes distribuées de la pioche
+    const newDeck = deck.slice(14);
+
+    // on prend la premiere carte de la pile pour la poser sur la table
+    const table = newDeck[0];
+    table.visible = true;
+    // on enleve la carte de la pile
+    newDeck.shift();
+
+    // affichage des cartes dans la console pour vérifier
+    console.log(playerHand);
+    console.log(botHand);
+    console.log(newDeck);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Page de jeu - à modifier</Text>
+
+        <Text style={styles.text}>Main du bot</Text>
+        <ScrollView horizontal style={styles.cardContainer}>
+          {botHand.map((card, index) => (
+            <Card key={index} color={card.color} value={card.value} visible={card.visible} />
+          ))}
+        </ScrollView>
+
+        <View style={styles.hr} />
+        <Card color={table.color} value={table.value} visible={table.visible} />
+        <View style={styles.hr} />
+
+        <Text style={styles.text}>Main du joueur</Text>
+        <ScrollView horizontal style={styles.cardContainer}>
+          {playerHand.map((card, index) => (
+            <Card key={index} color={card.color} value={card.value} visible={card.visible} />
+          ))}
+        </ScrollView>
+
+    <View style={styles.endGame}>
+
+      <View style={styles.hr} />
+
       <Button color="red" title="Abandonner" onPress={handleAbandonGame} />
     </View>
+
+    </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#333',
   },
   text: {
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
+    marginBottom: 20, // Espacement entre le texte et les cartes
+  },
+  hr: {
+    width: '100%', // Largeur de la ligne
+    height: 1,    // Épaisseur de la ligne
+    backgroundColor: 'white', // Couleur de la ligne
+    marginVertical: 20, // Espace autour de la ligne
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    maxHeight: 120, // Hauteur maximale de la ScrollView
+    bottom: 0, // Aligner les cartes en bas
+  },
+  endGame: {
+    position: 'absolute', // Position absolue pour le footer
+    bottom: 0, // En bas de l'écran
+    width: '100%', // Prend toute la largeur    
   },
 });
---
+++ components/uno/Card.tsx
@@ -0,0 +1,117 @@
+// Card.tsx
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+
+// Définir l'interface des props pour le composant Carte
+interface CardProps {
+  color: string;
+  // 0 a 9, draw, reverse, skip
+  value: string;
+  visible: boolean;
+}
+
+// Créer un objet pour mapper chaque carte à son image
+const cardImages: { [key: string]: any } = {
+  // Cartes rouges
+  'Red-0': require('@/assets/images/unoPack/Red_0.png'),
+  'Red-1': require('@/assets/images/unoPack/Red_1.png'),
+  'Red-2': require('@/assets/images/unoPack/Red_2.png'),
+  'Red-3': require('@/assets/images/unoPack/Red_3.png'),
+  'Red-4': require('@/assets/images/unoPack/Red_4.png'),
+  'Red-5': require('@/assets/images/unoPack/Red_5.png'),
+  'Red-6': require('@/assets/images/unoPack/Red_6.png'),
+  'Red-7': require('@/assets/images/unoPack/Red_7.png'),
+  'Red-8': require('@/assets/images/unoPack/Red_8.png'),
+  'Red-9': require('@/assets/images/unoPack/Red_9.png'),
+
+  'Red-skip': require('@/assets/images/unoPack/Red_Skip.png'),
+  'Red-reverse': require('@/assets/images/unoPack/Red_Reverse.png'),
+  'Red-draw': require('@/assets/images/unoPack/Red_Draw.png'),
+  
+  // Cartes vertes
+  'Green-0': require('@/assets/images/unoPack/Green_0.png'),
+  'Green-1': require('@/assets/images/unoPack/Green_1.png'),
+  'Green-2': require('@/assets/images/unoPack/Green_2.png'),
+  'Green-3': require('@/assets/images/unoPack/Green_3.png'),
+  'Green-4': require('@/assets/images/unoPack/Green_4.png'),
+  'Green-5': require('@/assets/images/unoPack/Green_5.png'),
+  'Green-6': require('@/assets/images/unoPack/Green_6.png'),
+  'Green-7': require('@/assets/images/unoPack/Green_7.png'),
+  'Green-8': require('@/assets/images/unoPack/Green_8.png'),
+  'Green-9': require('@/assets/images/unoPack/Green_9.png'),
+
+  'Green-skip': require('@/assets/images/unoPack/Green_Skip.png'),
+  'Green-reverse': require('@/assets/images/unoPack/Green_Reverse.png'),
+  'Green-draw': require('@/assets/images/unoPack/Green_Draw.png'),
+
+  // Cartes bleues
+  'Blue-0': require('@/assets/images/unoPack/Blue_0.png'),
+  'Blue-1': require('@/assets/images/unoPack/Blue_1.png'),
+  'Blue-2': require('@/assets/images/unoPack/Blue_2.png'),
+  'Blue-3': require('@/assets/images/unoPack/Blue_3.png'),
+  'Blue-4': require('@/assets/images/unoPack/Blue_4.png'),
+  'Blue-5': require('@/assets/images/unoPack/Blue_5.png'),
+  'Blue-6': require('@/assets/images/unoPack/Blue_6.png'),
+  'Blue-7': require('@/assets/images/unoPack/Blue_7.png'),
+  'Blue-8': require('@/assets/images/unoPack/Blue_8.png'),
+  'Blue-9': require('@/assets/images/unoPack/Blue_9.png'),
+
+  'Blue-skip': require('@/assets/images/unoPack/Blue_Skip.png'),
+  'Blue-reverse': require('@/assets/images/unoPack/Blue_Reverse.png'),
+  'Blue-draw': require('@/assets/images/unoPack/Blue_Draw.png'),
+
+  // Cartes jaunes
+  'Yellow-0': require('@/assets/images/unoPack/Yellow_0.png'),
+  'Yellow-1': require('@/assets/images/unoPack/Yellow_1.png'),
+  'Yellow-2': require('@/assets/images/unoPack/Yellow_2.png'),
+  'Yellow-3': require('@/assets/images/unoPack/Yellow_3.png'),
+  'Yellow-4': require('@/assets/images/unoPack/Yellow_4.png'),
+  'Yellow-5': require('@/assets/images/unoPack/Yellow_5.png'),
+  'Yellow-6': require('@/assets/images/unoPack/Yellow_6.png'),
+  'Yellow-7': require('@/assets/images/unoPack/Yellow_7.png'),
+  'Yellow-8': require('@/assets/images/unoPack/Yellow_8.png'),
+  'Yellow-9': require('@/assets/images/unoPack/Yellow_9.png'),
+
+  'Yellow-skip': require('@/assets/images/unoPack/Yellow_Skip.png'),
+  'Yellow-reverse': require('@/assets/images/unoPack/Yellow_Reverse.png'),
+  'Yellow-draw': require('@/assets/images/unoPack/Yellow_Draw.png'),
+
+  // Cartes spéciales
+  'Wild-': require('@/assets/images/unoPack/Wild.png'),
+  'Wild-draw': require('@/assets/images/unoPack/Wild_Draw.png'),
+
+  'back': require('@/assets/images/unoPack/Deck.png'),
+};
+
+const Card: React.FC<CardProps> = ({ color, value, visible }) => {
+  // Créer une clé unique pour chaque carte
+  const cardKey = `${color.charAt(0).toUpperCase() + color.slice(1)}-${value}`; // La première lettre de color devient une majuscule
+
+  let image = null;
+  if (visible) {
+    image = cardImages[cardKey];
+  } else {
+    image = cardImages['back'];
+  }
+
+  if (!image) {
+    return null; // Si l'image n'existe pas, on ne rend rien
+  }
+
+  return (
+    <View>
+      <Image source={image} style={styles.cardImage} />
+    </View>
+  );
+};
+
+// Styles
+const styles = StyleSheet.create({
+  cardImage: {
+    width: 80,
+    height: 120,
+    margin: 5,
+  },
+});
+
+export default Card;
